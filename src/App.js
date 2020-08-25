8fb5f688c7ed63f9957ab851e3edfd1ec8c6b417
najmus2808 <najmus2808@gmail.com>
2020-08-25 13:15:00 -0700

    logo set

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-       
-      </header>
+    <div>
+      <Header></Header>
     </div>
   );
 }
